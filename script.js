3f119e5c3378eb25f9293b59f875f03bdd81351a
rubinduck <rubinduck@protonmail.com>
2021-11-16 23:17:27 +0100

    Replace function declarations with arrow functions

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ function hasPlayerWon(humanMove){
     return compareMoves(humanMove, machineMove) === 1;
 }
 
+const getRandomMove = () =>
+    MOVES[randomInRange(0, MOVES.length)];
+
+// returns random number including min, not including max
+const randomInRange = (min, max) =>
+    Math.floor(Math.random() * (max - min) + min);
+
 // returns 0 for equal, 1 for move1 > move2 and -1 for move1 < move2
 function compareMoves(move1, move2){
     if (!MOVES.includes(move1)) throw new Error(`${move1} is not correct move`);
@@ -20,15 +27,6 @@ function compareMoves(move1, move2){
         case 'Scissors':
             return move2 === 'Paper' ? 1 : -1;
     }
-}
-
-function genRandomMove(){
-    return MOVES[randomInRange(0, MOVES.length)];
-}
-
-// returns random number including min, not including max
-function randomInRange(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
 }
 
 
