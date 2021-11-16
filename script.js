8be5d13288929e3aa409b3e746e7648de319260d
rubinduck <rubinduck@protonmail.com>
2021-11-16 03:22:46 +0100

    Add game functionality script

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-const MOVES = ['Rock', 'Paper', 'Scissors'];
+'use strict'
+import {hasPlayerWon} from '/rock-paper-scissors.js';
 
-function hasWon(humanMove){
-    machineMove = genRandomMove();
-    return compareMoves(humanMove, machineMove) === 1;
-}
+function main(){
+    const gameInvitation = document.getElementById('game-invitation');
+    const shapesContainer = document.getElementById('shapes-container');
+    const winMessage = document.getElementById('win-message');
+    const lossMessage = document.getElementById('loss-message');
+    const playAgainButton = document.getElementById('play-again-button');
+    const gameItems = Array.from(document.getElementsByClassName('game-shape'));
 
-// returns 0 for equal, 1 for move1 > move2 and -1 for move1 < move2
-function compareMoves(move1, move2){
-    if (!MOVES.includes(move1)) throw new Exception(`${move1} is not correct move`);
-    if (!MOVES.includes(move2)) throw new Exception(`${move2} is not correct move`);
-    if (move1 === move2)
-        return 0;
-    switch (move1) {
-        case 'Rock':
-            return move2 === 'Scissors' ? 1 : -1;
-        case 'Paper':
-            return move2 === 'Rock' ? 1 : -1;
-        case 'Scissors':
-            return move2 === 'Paper' ? 1 : -1;
+    gameItems.forEach(item =>
+        item.addEventListener('click', event => playRound(event.target.value)));
+    playAgainButton.addEventListener('click', event => startNewGame());
+    startNewGame();
+
+    function startNewGame(){
+        show(gameInvitation);
+        show(shapesContainer);
+        hide(winMessage);
+        hide(lossMessage);
+        hide(playAgainButton);
+    }
+
+    function playRound(playersItem){
+        const playerWon = hasPlayerWon(playersItem);
+        hide(gameInvitation);
+        hide(shapesContainer);
+        if (playerWon)
+            show(winMessage);
+        else
+            show(lossMessage);
+        show(playAgainButton)
+    };
+
+    function hide(element){
+        element.classList.add('hidden');
+    }
+
+    function show(element){
+        element.classList.remove('hidden')
     }
 }
 
-function genRandomMove(){
-    return MOVES[randomInRange(0, MOVES.length)];
-}
-
-// returns random number including min, not including max
-function randomInRange(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
+main()
